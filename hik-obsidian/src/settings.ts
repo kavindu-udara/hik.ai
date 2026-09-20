d7d89398fdf859aca434bc179d3056511a34296b
kavindu-udara <karunasinghesampath38@gmail.com>
2026-09-20 06:15:27 +0530

    settings updated

--- hik-obsidian/src/settings.ts
+++ hik-obsidian/src/settings.ts
@@ -1,36 +1,65 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from './main';
+import HikPlugin from './main';
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface HikSettings {
+	apiKey: string;
+	baseUrl: string;
+	model: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default',
+export const DEFAULT_SETTINGS: HikSettings = {
+	apiKey: '',
+	baseUrl: 'http://localhost:3000',
+	model: 'qwen2.5-coder-7b-instruct',
 };
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class HikSettingTab extends PluginSettingTab {
+	plugin: HikPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HikPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const { containerEl } = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc("It's a secret")
+			.setName('Hik API Key')
+			.setDesc('Your hik_xxx API key generated from the Hik dashboard.')
 			.addText((text) =>
 				text
-					.setPlaceholder('Enter your secret')
-					.setValue(this.plugin.settings.mySetting)
+					.setPlaceholder('hik_...')
+					.setValue(this.plugin.settings.apiKey)
 					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+						this.plugin.settings.apiKey = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+
+		new Setting(containerEl)
+			.setName('Backend URL')
+			.setDesc('The URL of your Hik backend server.')
+			.addText((text) =>
+				text
+					.setPlaceholder('http://localhost:3000')
+					.setValue(this.plugin.settings.baseUrl)
+					.onChange(async (value) => {
+						this.plugin.settings.baseUrl = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+
+		new Setting(containerEl)
+			.setName('Default Model')
+			.setDesc('The model to use for chat requests.')
+			.addText((text) =>
+				text
+					.setPlaceholder('qwen2.5-coder-7b-instruct')
+					.setValue(this.plugin.settings.model)
+					.onChange(async (value) => {
+						this.plugin.settings.model = value;
 						await this.plugin.saveSettings();
 					}),
 			);
